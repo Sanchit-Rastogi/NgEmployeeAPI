75c6509b6208f5c8d7374fd7ee5c75c2e6609d07
Sanchit Rastogi <8958698863sr@gmail.com>
2021-05-17 11:57:20 +0530

    initial commit

---
+++ EmployeeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeRepository employeeRepository;
+
+        public EmployeeController()
+        {
+            employeeRepository = new EmployeeRepository();
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return employeeRepository.GetAll();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return employeeRepository.GetById(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Employee emp)
+        {
+            if (ModelState.IsValid)
+                employeeRepository.Add(emp);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Employee emp)
+        {
+            emp.EmployeeId = id;
+            if (ModelState.IsValid)
+                employeeRepository.Update(emp);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            employeeRepository.Delete(id);
+        }
+    }
+}
---
+++ EmployeeAPI/Models/EmployeeRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace EmployeeAPI.Models
+{
+    public class EmployeeRepository
+    {
+        private string connectionString;
+
+        public EmployeeRepository()
+        {
+            connectionString = "Data Source=.;Initial Catalog=EmployeeDB;user=sa;password=Sanchit123@sql#;Trusted_Connection=false";
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+
+        public void Add(Employee emp)
+        {
+            using IDbConnection dbConnection = Connection;
+            string sQuery = "INSERT INTO Employee(FirstName, LastName, PreferredName, JobTitle, Department, PhoneNumber, Email,Office, SkypeId ) VALUES (@FirstName, @LastName, @PreferredName, @JobTitle, @Department, @PhoneNumber, @Email, @Office, @SkypeId)";
+            dbConnection.Open();
+            dbConnection.Execute(sQuery, emp);
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            using IDbConnection dbConnection = Connection;
+            string sQuery = "SELECT * FROM Employee";
+            dbConnection.Open();
+            return dbConnection.Query<Employee>(sQuery);
+        }
+
+        public Employee GetById(int id)
+        {
+            using IDbConnection dbConnection = Connection;
+            string sQuery = "SELECT * FROM Employee WHERE EmployeeId=@Id";
+            dbConnection.Open();
+            return dbConnection.Query<Employee>(sQuery, new {Id = id}).FirstOrDefault();
+        }
+
+        public void Delete(int id)
+        {
+            using IDbConnection dbConnection = Connection;
+            string sQuery = "DELETE FROM Employee WHERE EmployeeId=@Id";
+            dbConnection.Open();
+            dbConnection.Execute(sQuery, new { Id = id });
+        }
+
+        public void Update(Employee emp)
+        {
+            using IDbConnection dbConnection = Connection;
+            string sQuery = "UPDATE Employee SET FirstName=@FirstName, LastName=@LastName, PreferredName=@PreferredName, JobTitle=@JobTitle, Department=@Department, PhoneNumber=@PhoneNumber, Email=@Email, Office=@Office, SkypeId=@SkypeId WHERE EmployeeId=@EmployeeId";
+            dbConnection.Open();
+            dbConnection.Query(sQuery,emp);
+        }
+    }
+}
